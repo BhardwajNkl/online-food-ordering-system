ff1838116061141fdf5e0aa47448fa4de3c74d1a
BhardwajNkl <nikhilbhardwaj91099@gmail.com>
2024-07-30 11:10:19 +0530

    initial commit

---
+++ src/test/java/dev/bhardwaj/food_order/FoodOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bhardwaj.food_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
